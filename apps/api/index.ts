bca5c4f6070d1b0fedec58d5c54d81708df5acb6
KuldeepJha5176 <ranacjha@gmail.com>
2025-03-16 23:09:12 +0530

    updated imports

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { authMiddleware } from "./middleware";
+import { prismaClient } from "db/client";
+
+const app  = express();
+
+
+app.post("/api/vi/website", authMiddleware , (req,res) => {
+    const userId = req.userId
+
+    prismaClient.user.create({
+
+    })
+})
+app.get("/api/vi/websites/status", authMiddleware ,  (req,res) => {
+
+})
+app.get("/api/vi/websites", authMiddleware, (req,res) =>{
+
+})
+app.delete("/api/vi/website/",authMiddleware, (req,res) =>{
+
+})
+
+app.listen(3000);
