ecffce260397c8e56d00dd81bbe61f823c44d204
KuldeepJha5176 <you@example.com>
2025-03-20 03:23:07 +0530

    added clerk and shadcn nextjs

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,16 +1,12 @@
 import express from "express";
 import { authMiddleware } from "./middleware";
-import { prismaClient } from "../../prisma/client";
+import { prismaClient } from "db/client";
 
 const app  = express();
 
 
 app.post("/api/vi/website", authMiddleware , (req,res) => {
-    const userId = req.userId
-
-    prismaClient.user.create({
-
-    })
+ 
 })
 app.get("/api/vi/websites/status", authMiddleware ,  (req,res) => {
 
--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,9 +1,20 @@
-import type { NextFunction , Request, Response } from "express";
+// import type { NextFunction , Request, Response } from "express";
 
-export function authMiddleware(req: Request , res: Response, next: NextFunction){
-    const authHeader =  req.headers['authorization'];
+// export function authMiddleware(req: Request , res: Response, next: NextFunction){
+//     const authHeader =  req.headers['authorization'];
 
-    req.userId = "1";
-    next()
-}
-   
+//     req.userId = "1";
+//     next()
+// }
+import {clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
